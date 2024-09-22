9709cc7c34a6ee321ca53c8283b765cf178d59a9
Keyboard Destroyer <grudko2003@gmail.com>
2024-09-22 17:04:25 +0300

    GAME CORE Initialization

---
+++ Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerMover : MonoBehaviour
+{
+    [SerializeField, Range(0f, 10f)] private float fPlayerSpeed = 0.0f;
+    [SerializeField, Range(0f, 1f)] private float fPlayerPositionBias;
+    [Header("Object dependencies")]
+    [SerializeField] private Camera mainCamera;
+
+    private Vector3 vDesiredPosition = Vector2.zero;
+
+    #region PRIVATES
+    private void movePlayerToPoint(Vector2 vPoint)
+    {
+        vDesiredPosition = vPoint;
+    }
+    #endregion
+
+    private void Start()
+    {
+        if (!mainCamera)
+            mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Vector2.Distance(transform.position, vDesiredPosition) > fPlayerPositionBias)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, vDesiredPosition, Time.deltaTime * fPlayerSpeed);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mouseSceenPosition = Input.mousePosition;
+            Vector3 mouseWorldPosition = mainCamera.ScreenToWorldPoint(mouseSceenPosition);
+
+            movePlayerToPoint(mouseWorldPosition);
+            Debug.Log(mouseWorldPosition);
+        }
+    }
+
+    protected void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawWireSphere(transform.position, fPlayerPositionBias);
+    }
+}
